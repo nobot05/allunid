5089f1341a0d97f8a750a1aa28e575d3a06ad357
mauerbauertraurigkeit <nour20hk@gmail.com>
2021-12-20 22:31:10 +0200

    commit

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import './shop.css'
 
 const Shop = () => {
     return (
-        <div className="container">
-            <h1 className="text-center" style={{paddingTop: "30%"}}>
-                Shop
-            </h1>
+        <div>
+            <div className='gradied'>hoi</div>
+            {/* <div className="container">
+                <h1 className="text-center" style={{paddingTop: "30%"}}>
+                    Shop
+                </h1>
+            </div> */}
         </div>
     )
 }
