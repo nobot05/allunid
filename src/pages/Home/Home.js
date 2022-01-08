5778d5624ee81fe9778f6296aacc8f28d9216b21
mauerbauertraurigkeit <nour20hk@gmail.com>
2022-01-08 12:13:23 +0200

    commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import MainCarousel from "../../Components/Carousel/MainCarousel";
 import "../../Components/homeStyle/homeStyle.css";
 import "./home.css"
@@ -19,11 +20,20 @@ const Home = () => {
       </section>
       <div className="logeen">
         <h1 className="titre">Become part of the AllUnid Family!</h1>
-        <button class="button-three">Login!</button>
-        <div class="wrap">
-  <h1>Squishy-ish Button</h1>
-  <input class="btn" type="button" />
-</div>
+        <div className="joinBtn">
+          <NavLink to="/login">
+            <button class="button-three">Login!</button>
+          </NavLink>
+          <NavLink to="/signup">
+            <button class="button-three">Signup!</button>
+          </NavLink>
+        </div>
+        {/* <div class="wrap">
+            <h1>Squishy-ish Button</h1>
+            <input class="btn" type="button" />
+            </div> */}
+
+
       </div>
     </div>
   );
